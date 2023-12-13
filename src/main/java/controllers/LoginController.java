1d36b9c16af6c3be243df541538cbaf093fe93bf
smiley <smileyfineart@gmail.com>
2023-12-13 15:05:56 +0000

    dexieme commit

---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package controllers;
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
